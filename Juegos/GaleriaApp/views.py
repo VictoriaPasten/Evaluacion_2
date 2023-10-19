0668dd7b6705f2c73786e089c241e1364072b2f3
Victoria <victoria.pasten04@inacapmail.cl>
2023-10-19 10:24:58 -0300

    app 2 lista

--- Juegos/GaleriaApp/views.py
+++ Juegos/GaleriaApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    videojuegos = [
+        {'name': 'The Legend of Zelda', 'genero': 'Acción-Aventura', 'plataforma': 'Varias', 'año': '1986','desarrollador':'Nintendo' ,'image': 'zelda.jpg'},
+        {'name': 'Super Mario Bros', 'genero': 'Plataformas', 'plataforma': 'Varias', 'año': '1985','desarrollador':'Nintendo', 'image': 'mario.jpg'},
+        {'name': 'Minecraft', 'genero': 'Sandbox', 'plataforma': 'Varias', 'año': '2011', 'desarrollador':'Mojang','image': 'minecraft.jpg'},
+    ]
+
+    selected_game = request.GET.get('game', 'The Legend of Zelda')
+
+    context = {
+        'videojuegos': videojuegos,
+        'selected_game': next(filter(lambda x: x['name'] == selected_game, videojuegos), None)
+    }
+
+    return render(request, 'GaleriaApp/index.html', context)
--- Juegos/Juegos/urls.py
+++ Juegos/Juegos/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from GaleriaApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('juego/<str:juego_id>/', views.index, name='juego'),
 ]
